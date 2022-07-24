b831b8b99fc82d31a167a1cb9eb138e37d45b28d
abusabir <sabir.hossain@ibcs-primax.com>
2022-07-24 23:52:31 +0600

    Change Code

--- app/Services/PersonService.php
+++ app/Services/PersonService.php
@@ -16,30 +16,34 @@ class PersonService
   public function getPersonsData($request)
   {
     // Set parameters into session 
-    if(isset($request->year) && isset($request->month)){
+    if(isset($request->year)){
       Session::put('year', $request->year);
+    }
+    if(!isset($request->year) && !isset($request->page)){
+      Session::put('year', '');
+    }
+    if(isset($request->month)){
       Session::put('month', $request->month);
     }
-    if(!isset($request->year) && !isset($request->month) && !isset($request->page)){
-      Session::put('year', '');
+    if(!isset($request->month) && !isset($request->page)){
       Session::put('month', '');
     }
     $year = Session::get('year');
     $month = Session::get('month');
+
     // Set data into redies
     $key = $year.'-'.$month;
     if(!Redis::exists($key)){
         $data = Person::query();
-        if($request->year) {
-            $data = $data->whereYear('birthdate', '=', $request->year);
+        if($year) {
+            $data = $data->whereYear('birthdate', '=', $year);
         }
-        if($request->year) {
-            $data = $data->whereMonth('birthdate', '=', $request->month);
+        if($month) {
+            $data = $data->whereMonth('birthdate', '=', $month);
         }
         $data = $data->get();
         Redis::set($key,  $data, 'EX', 60);
     }
-
     $data = collect(json_decode(Redis::get($key)));
     $data = $data->paginate(20);
     return $data;
